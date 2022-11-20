5cd7a2cef70ddad04c0b443fed5302c5af09ef93
amarnath-arch <amarnath.4147ac@gmail.com>
2022-11-20 15:24:03 +0530

    deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,11 +3,18 @@ const { ethers } = require("hardhat");
 const main = async()=>{
     const [deployer] = await ethers.getSigners();
 
+    const Token = await ethers.getContractFactory("CryptoPunkz");
+    const token = await Token.deploy();
+
+    await token.deployed();
+
+    console.log(`ERC721 Token deployed to ${token.address}`);
+
     const FractionalNFT  = await ethers.getContractFactory("FractionalNFT");
     const fractionalNFT = await FractionalNFT.deploy('FNFT','1000000000',5);
     await fractionalNFT.deployed();
 
-    console.log(`Deployed to ${fractionalNFT.address}`);
+    console.log(`Fractional NFT Contract Deployed to ${fractionalNFT.address}`);
 
     const balance = await fractionalNFT.balanceOf(deployer.address);
     console.log(balance.toString());
